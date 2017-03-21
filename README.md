b954e01f83af32446218ea033b46f1459844d159
Jesus Seijas <jseijas@gmail.com>
2017-03-21 22:56:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# generator-flowbot [![NPM version][npm-image]][npm-url] [![Dependency Status](https://david-dm.org/jseijas/generator-flowbot/status.svg)](https://david-dm.org/jseijas/generator-flowbot)
+
+> Generates flow chatbots in an easy way.
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-flowbot using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-flowbot
+```
+
+Then generate your new project:
+
+```bash
+yo flowbot
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Jesus Seijas]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-flowbot.svg
+[npm-url]: https://npmjs.org/package/generator-flowbot
+[travis-image]: https://travis-ci.org/nodejsbcn/generator-flowbot.svg?branch=master
+[travis-url]: https://travis-ci.org/nodejsbcn/generator-flowbot
+[daviddm-image]: https://david-dm.org/nodejsbcn/generator-flowbot.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/nodejsbcn/generator-flowbot
---
+++ generators/app/index.js
@@ -0,0 +1,79 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var path = require('path');
+var fs = require('fs');
+var glob = require('glob');
+
+module.exports = yeoman.Base.extend({
+
+  /**
+   * Ask general questions to generate the flowbot app.
+   */
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the stupendous ' + chalk.red('generator-flowbot') + ' generator!'
+    ));
+
+    var defAppName = path.basename(process.cwd());
+
+    var prompts = [{
+      type: 'input',
+      name: 'appName',
+      message: 'What\'s the name of your application?',
+      default: defAppName
+    },{
+      type: 'input',
+      name: 'appFolder',
+      message: 'What\'s the name of the folder? (Will be created)',
+      default: defAppName
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      this.props = props;
+    }.bind(this));
+  },
+
+  /**
+   * Grite the files from the template folder.
+   */
+  writing: function () {
+    var done = this.async();
+    done();
+    // var root = path.join(this.destinationRoot(), this.props.appName);
+    // if (!fs.existsSync(root)) {
+    //   this.log.create(this.props.appName + '/');
+    //   fs.mkdirSync(root);
+    // }
+    // this.destinationRoot(root);
+    // this.log();
+    // glob('**', {dot:true, cwd: this.sourceRoot()}, function (er, files) {
+    //   for (var i = 0; i < files.length; i++) {
+    //     var fileName = files[i];
+    //     var targetName = fileName.replace(/^_/, '');
+    //     if (fileName.startsWith('_')) {
+    //       this.template(fileName, targetName, this.props);
+    //     } else {
+    //       this.fs.copy(
+    //         this.templatePath(fileName),
+    //         this.destinationPath(targetName));
+    //     }
+    //   }
+    //   done();
+    // }.bind(this));
+  },
+
+  /**
+   * Install dependencies.
+   */
+  install: function () {
+    this.installDependencies({ bower: false, npm: true, callback: function() {
+      this.log('\r\n');
+      this.log('Your flowbot project is now created. You can use the following commands to get going');
+      this.log(chalk.green('    cd "'+this.props.appFolder+'"'));
+      this.log(chalk.green('    npm start'));
+    }.bind(this) });
+  }
+
+});
