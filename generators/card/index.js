9dd40a89966e5d424755b82a043d8fa5787160fb
Jesus Seijas <jesus.seijas@axa-groupsolutions.com>
2017-03-22 12:33:10 +0100

    Adding prompts

--- generators/card/index.js
+++ generators/card/index.js
@@ -12,7 +12,7 @@ module.exports = yeoman.Base.extend({
     var cardFileName = path.join(this.destinationRoot(), '/bot/cards/cards.json');
     var cards = require(cardFileName);
     this.data.cards = cards;
-    done();
+    return done();
   },
   inputName: function () {
     var done = this.async();
@@ -24,7 +24,7 @@ module.exports = yeoman.Base.extend({
       this.log(answers.name);
       this.data.card = {};
       this.data.card.name = answers.name;
-      done();
+      return done();
     });
   },
   checkName: function () {
@@ -34,7 +34,7 @@ module.exports = yeoman.Base.extend({
         this.env.error("Sorry. A card with the same name already exists");
       }
     }
-    done();
+    return done();
   },
   inputType: function () {
     var done = this.async();
@@ -46,7 +46,28 @@ module.exports = yeoman.Base.extend({
     }];
     this.prompt(prompts).then((answers) => {
       this.data.card.type = answers.cardType.toLowerCase();
-      done();
+      return done();
+    });
+  },
+  inputPromptType: function() {
+    var done = this.async();
+    var validTypes = ['prompt'];
+    if (validTypes.indexOf(this.data.card.type) < 0) {
+      return done();
+    }
+    var prompts = [{
+      type: 'list',
+      name: 'promptType',
+      message: 'Choose a prompt type:',
+      choices: ['Text', 'Number', 'Time', 'Attachment', 'Choice', 'Menu']
+    }];
+    this.prompt(prompts).then((answers) => {
+      this.data.card.prompt = answers.promptType.toLowerCase();
+      if (this.data.card.prompt === 'menu') {
+        this.data.card.isMenu = true;
+        this.data.card.prompt = 'choice';
+      }
+      return done();
     });
   },
   inputTitle: function () {
@@ -60,10 +81,10 @@ module.exports = yeoman.Base.extend({
       }];
       this.prompt(prompts).then((answers) => {
         this.data.card.title = answers.title;
-        done();
+        return done();
       });
     } else {
-      done();
+      return done();
     }
   },
   inputSubtitle: function () {
@@ -79,10 +100,10 @@ module.exports = yeoman.Base.extend({
         if (answers.subtitle && answers.subtitle !== '') {
           this.data.card.subtitle = answers.subtitle;
         }
-        done();
+        return done();
       });
     } else {
-      done();
+      return done();
     }
   },
   inputImage: function () {
@@ -109,10 +130,10 @@ module.exports = yeoman.Base.extend({
         } else {
           this.data.card.image = answers.image;
         }
-        done();
+        return done();
       });
     } else {
-      done();
+      return done();
     }
   },
   inputText: function () {
@@ -126,17 +147,17 @@ module.exports = yeoman.Base.extend({
       }];
       this.prompt(prompts).then((answers) => {
         this.data.card.text = answers.cardText;
-        done();
+        return done();
       });
     } else {
-      done();
+      return done();
     }
   },
   inputButton: function() {
     var done = this.async();
-    var validTypes = ['text', 'hero', 'prompt'];
+    var validTypes = ['hero'];
     if (validTypes.indexOf(this.data.card.type) < 0) {
-      done();
+      return done();
     }
     var prompts = [{
       type: 'text',
@@ -186,9 +207,113 @@ module.exports = yeoman.Base.extend({
           }
         });
       } else {
-        done();
+        return done();
       }
     });
+  },
+  inputCarouselCard: function() {
+    var done = this.async();
+    var validTypes = ['carousel'];
+    if (validTypes.indexOf(this.data.card.type) < 0) {
+      return done();
+    }
+    var prompt = {
+      type: 'list',
+      name: 'card',
+      message: 'Choose a card to add to the Carousel: ',
+      choices: ['Finish choosing cards']
+    }
+    for (var i = 0; i < this.data.cards.length; i++) {
+      prompt.choices.push(this.data.cards[i].name);
+    }
+    this.prompt([prompt]).then((answers) => {
+      if (answers.card === 'Finish choosing cards') {
+        return done();
+      }
+      if (!this.data.card.cards) {
+        this.data.card.cards = [];
+      }
+      this.data.card.cards.push(answers.card);
+      this.inputCarouselCard();
+    });
+  },
+  inputVariableName: function () {
+    var done = this.async();
+    var validTypes = ['prompt'];
+    console.log(this.data.card.type);
+    if (validTypes.indexOf(this.data.card.type) < 0) {
+      return done();
+    }
+    this.prompt([{
+      type: 'input',
+      name: 'variableName',
+      message: 'Tell me the variable name (user.name if is stored for the user, dialog.name if is temporal for this dialog):'
+    }]).then((answers) => {
+      this.data.card.variable = answers.variableName;
+      return done();
+    });
+  },
+  inputChoice: function() {
+    var done = this.async();
+    if (this.data.card.prompt && this.data.card.prompt === 'choice' && !this.data.card.isMenu) {
+      var prompts = [{
+        type: 'text',
+        name: 'choiceCaption',
+        message: 'Caption of the choice (Enter to finish): '
+      }];
+      this.prompt(prompts).then((answers) => {
+        if (!answers.choiceCaption || answers.choiceCaption === '') {
+          return done();
+        }
+        this.data.currentOption = { text: answers.choiceCaption }
+        var prompts2 = [{
+          type: 'text',
+          name: 'choiceValue',
+          message: 'Value returned by this choice: '
+        }];
+        this.prompt(prompts2).then((answers) => {
+          if (!this.data.card.options) {
+            this.data.card.options = [];
+          }
+          this.data.currentOption.tag = answers.choiceValue;
+          this.data.card.options.push(this.data.currentOption);
+          return this.inputChoice();
+        });
+      });
+    } else {
+      return done();
+    }
+  },
+  inputMenu: function() {
+    var done = this.async();
+    if (this.data.card.prompt && this.data.card.prompt === 'choice' && this.data.card.isMenu) {
+      var prompts = [{
+        type: 'text',
+        name: 'choiceCaption',
+        message: 'Caption of the choice (Enter to finish): '
+      }];
+      this.prompt(prompts).then((answers) => {
+        if (!answers.choiceCaption || answers.choiceCaption === '') {
+          return done();
+        }
+        this.data.currentOption = { text: answers.choiceCaption }
+        var prompts2 = [{
+          type: 'text',
+          name: 'choiceValue',
+          message: 'Dialog to be executed by this choice (endDialog means that this is a quit menu option): '
+        }];
+        this.prompt(prompts2).then((answers) => {
+          if (!this.data.card.options) {
+            this.data.card.options = [];
+          }
+          this.data.currentOption.tag = answers.choiceValue;
+          this.data.card.options.push(this.data.currentOption);
+          return this.inputMenu();
+        });
+      });
+    } else {
+      return done();
+    }
   },
   printCards: function () {
     this.data.cards.push(this.data.card);
